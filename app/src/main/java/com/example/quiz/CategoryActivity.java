270a8f28241ecd2020faba840532ea3c1db9b585
doniyor <doniyorprince95@gmail.com>
2023-01-28 16:29:29 +0500

    speaking girl added

--- app/src/main/java/com/example/quiz/CategoryActivity.java
+++ app/src/main/java/com/example/quiz/CategoryActivity.java
@@ -3,8 +3,10 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        ImageView mImageViewFilling = (ImageView) findViewById(R.id.speak);
+        ((AnimationDrawable) mImageViewFilling.getBackground()).start();
+
     }
 
     public void PlaySkelet(View view) {
